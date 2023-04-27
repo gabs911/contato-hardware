79e10b7c24d9c0af39345c9d4a1542483f943716
Carol Breder <c.brederfigueiredo@gmail.com>
2023-04-27 09:25:53 -0300

    angel e parque tecnologico

--- scripts/performance/cena1/md.py
+++ scripts/performance/cena1/md.py
@@ -3,7 +3,7 @@ import serial
 import time
 import rtmidi
 
-serialPort = serial.Serial(port = "COM4", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port = "COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 serialString = ''
 
 midiout = rtmidi.MidiOut()
