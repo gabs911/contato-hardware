b2722326aea6f77d2e325474338f7d7644463db4
Carol Breder <c.brederfigueiredo@gmail.com>
2023-04-27 11:50:01 -0300

    27-04-2023

--- scripts/performance/inovatec.py
+++ scripts/performance/inovatec.py
@@ -8,7 +8,7 @@ COM7 - Testes
 '''
 #Alterar port de acordo com a saída bluetooth do contato
 
-serialPort = serial.Serial(port = "COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port = "COM5", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 serialString = ''
 
 midiout = rtmidi.MidiOut()
@@ -109,10 +109,10 @@ while(1):
     if(accel > 5000 and (time.time() - previousSoundEffectActiv >= soundeEffectInterval)):
         previousSoundEffectActiv = time.time()
         print("ACCEL DETECTED")
-        midiout.send_message([0x91,94,120]) #parametro da nota segundo numero do midiout.sed_message
+        midiout.send_message([0x91,69,120]) #parametro da nota segundo numero do midiout.sed_message
     
     if(time.time() - previousSoundEffectActiv >= soundEffectDuration):
         previousSoundEffect = time.time()
         #print("ACCEL SOUND EFFECT OFF")
 
-        midiout.send_message([0x81,94,120]) #nota tem que ta igual nos dois midiout.sed_message do accel
+        midiout.send_message([0x81,69,120]) #nota tem que ta igual nos dois midiout.sed_message do accel
