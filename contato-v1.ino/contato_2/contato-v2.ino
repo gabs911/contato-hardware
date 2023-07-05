9adf45a37cfeb68aed86d1016168d3055ff7bfec
c-breder <c.brederfigueiredo@gmail.com>
2023-07-05 20:52:25 -0300

    atualização

---
+++ contato-v1.ino/contato_2/contato-v2.ino
@@ -0,0 +1,153 @@
+
+#include "I2Cdev.h"
+#include "BluetoothSerial.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE 
+    #include "Wire.h"
+#endif
+
+
+MPU6050 mpu;
+BluetoothSerial SerialBT;
+
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 
+
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+float ypr_mod;
+// Vars do acionador
+int pressed = 0;
+int mediaAccel = 0;
+int referenciaAcionador = 0;
+
+volatile bool mpuInterrupt = false;     
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); 
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    referenciaAcionador = calibraAcionador();
+ 
+    SerialBT.begin("contato-02");
+    Serial.begin(115200);
+    
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6); //porque é 6?
+        mpu.PrintActiveOffsets();
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
+
+
+
+void loop() {
+    if (!dmpReady) return;
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { 
+          mpu.dmpGetQuaternion(&q, fifoBuffer);
+          mpu.dmpGetAccel(&aa, fifoBuffer);
+          mpu.dmpGetGravity(&gravity, &q);
+          mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+          mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+
+        for(int i = 0; i < 2; i++ )
+        {
+          ypr[i] = ypr[i] * 180/M_PI;
+        }
+    }
+    pressed = touchRead();
+    SerialBT.println("01/" + String(ypr[0])+'/' + String(ypr[1])+'/' + String(ypr[2])+'/' + String(aaWorld.z)+'/'+String(aaWorld.x)+'/'+String(aaWorld.y));
+    //Print na serial para debug
+    Serial.println("01/" + String(ypr_mod)+'/'+String(mediaAccel)+'/'+String(pressed));
+}
+
+int touchRead()
+{
+  int media = 0;
+  mediaAccel = 0;
+  for(int i=0; i< 10; i++)
+  {
+    media += touchRead(T3);
+    mediaAccel += aaReal.z;
+
+  }
+  media =  media/100;
+  mediaAccel = mediaAccel/100;
+  Serial.println(media);
+  
+  if(media < 20)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+
+}
